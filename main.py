2a0f325c90f455618d5b2078a50422abfc97a0ea
Ben Roig <bgroig@gmail.com>
2017-04-04 19:57:24 -0500

    Finished version of user-signup.

--- main.py
+++ main.py
@@ -16,6 +16,19 @@
 #
 import webapp2
 import cgi
+import re
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(new_username):
+    return new_username and USER_RE.match(new_username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(new_password):
+    return new_password and PASS_RE.match(new_password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(new_email):
+    return new_email or EMAIL_RE.match(new_email)
 
 page_header = """
 <!DOCTYPE html>
@@ -23,6 +36,9 @@ page_header = """
 <head>
     <title>User-signup</title>
     <style type="text/css">
+        .label {
+            text-align: right;
+        }
         .error {
             color: red;
         }
@@ -38,102 +54,119 @@ page_footer = """
 </body>
 </html>
 """
+form = """
+    <form method="post">
+        <table>
+            <tbody>
+                <tr>
+                    <td>
+                        <label for="username">Username</label>
+                    </td>
+                    <td>
+                        <input name="username" type="text" value="{4}">
+                        <span class="error">{0}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="password">Password</label>
+                    </td>
+                    <td>
+                        <input name="password" type="password" value required>
+                        <span class="error">{1}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="password">Verify Password</label>
+                    </td>
+                    <td>
+                        <input name="verify" type="password" required />
+                        <span class="error">{2}</span>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="email">Email (optional)</label>
+                    </td>
+                    <td>
+                        <input name="email" type="email" value="{5}">
+                        <span class="error">{3}</span>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <input type="submit"/>
+    </form>
+        """
+
 
 class MainHandler(webapp2.RequestHandler):
 
     def get(self):
 
-        form = """
-        <form action="/add" method="post">
-            <table>
-                <tbody>
-                    <tr>
-                        <td>
-                            <label for="username">Username</label>
-                        </td>
-                        <td>
-                            <input name="username" type="text" value required />
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="password">Password</label>
-                        </td>
-                        <td>
-                            <input name="password" type="password" required />
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="password">Verify Password</label>
-                        </td>
-                        <td>
-                            <input name="verify" type="password" required />
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <label for="email">Email (optional)</label>
-                        </td>
-                        <td>
-                            <input name="email" type="email" value/>
-                            <span class="error"></span>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-            <input type="submit"/>
-        </form>
-            """
-
-        error = self.request.get("error")
-
-        if error:
-            error_esc = cgi.escape(error, quote=True)
-            error_element = '<p class="error">' + error_esc + '</p>'
-        else:
-            error_element = ''
-
-        main_content = form + error_element
-        content = page_header + main_content + page_footer
+        content = page_header + form.format("","","","","","") + page_footer
 
         self.response.write(content)
-
-class addUser(webapp2.RequestHandler):
-    """Handles request to '/add'
-        at www.user-signup.com/add
-    """
 
     def post(self):
 
-        new_username = self.request.get("username")
-        new_password = self.request.get("password")
-        password_verified = self.request.get("verify")
-        new_email = self.request.get("email")
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
 
-        if len(new_username) < 3 or ' ' in new_username:
-            self.redirect("/?error= That's not a valid username.".format(new_username))
 
-        if len(new_password) < 3:
-            self.redirect("/?error= That's not a valid password".format(new_password))
+        error1 = ""
+        error2 = ""
+        error3 = ""
+        error4 = ""
 
-        if password_verified != new_password:
-            self.redirect("/?error= Passwords don't match.".format(password_verified))
+        if not valid_username(username):
+            error1 = "That's not valid username."
 
-        if new_email:
-            if "@" and "." not in new_email:
-                self.redirect("/?error= That's not a valid email address.".format(new_email))
+        if not valid_password(password):
+            error2 = "That's not a valid password."
 
-        new_username = cgi.escape(new_username, quote=True)
+        if password != verify:
+            error3 = "Passwords don't match."
 
-        content = "<h1>Welcome, " + new_username + "!</h1>"
+        if email:
+            if "@" and "." not in email:
+                error4 = "That's not a valid email address."
+
+
+        if error1 or error2 or error3 or error4:
+            content = page_header + form.format(error1, error2, error3, error4, username, email) + page_footer
+
+        else:
+            content = "<h1>Welcome, " + username + "!</h1>"
+
+        username = cgi.escape(username, quote=True)
+
         self.response.write(content)
 
 
+
+        #if len(username) < 3 or ' ' in username:
+        #    self.redirect("/?error1= That's not a valid username.")
+
+        # if len(password) < 3:
+        #     self.redirect("/?error2= That's not a valid password.")
+        #
+        # if verify != password:
+        #     self.redirect("/?error3= Passwords don't match.")
+        #
+        # if email:
+        #     if "@" and "." not in email:
+        #         self.redirect("/?error4= That's not a valid email address.")
+
+
+
+
+
+
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/add', addUser)
+    ('/', MainHandler)
 ], debug=True)
